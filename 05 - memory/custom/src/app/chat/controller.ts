41b64dc5e4d1f6157c5d02035cd65f99e7416d33
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-08-12 03:44:14 +0530

    refactor: update chat handling to use userID for chat and ltm retrieval

--- 05 - memory/custom/src/app/chat/controller.ts
+++ 05 - memory/custom/src/app/chat/controller.ts
@@ -1,34 +1,43 @@
 // internal-imports
 import { openai } from '../../core/lib/openai.js';
 import { Chat } from '../../core/db/model/chat.js';
+import { LTM } from '../../core/db/model/ltm.js';
 import { Message } from '../../core/db/model/message.js';
 import { Role } from '../../core/config/role.js';
 import { SP } from '../../core/config/prompt.js';
 
 // external-imports
-import mongoose from 'mongoose';
 import type { Request, Response } from 'express';
 
 // controller for module
 export const controller = {
   // @controller POST /
   startChat: async (request: Request, response: Response) => {
-    // find the chat in db, if not found create a new one
-    const chat = await Chat.findByIdAndUpdate(
-      request.body.id ?? new mongoose.Types.ObjectId(),
+    // find the user's chat in db, if not found create a new one
+    const chat = await Chat.findOneAndUpdate(
+      { userID: request.body.userID },
       {},
       { returnDocument: 'after', upsert: true }
-    ).select('ltm _id');
+    )
+      .select('_id')
+      .lean();
+
+    // find the user's ltm in db, if not found create a new one
+    const ltm = await LTM.findOneAndUpdate(
+      { userID: request.body.userID },
+      {},
+      { returnDocument: 'after', upsert: true }
+    ).select('facts');
 
     // get the last 20 messages of the chat from db
-    const STM = await Message.find({ chatID: chat._id })
+    const stm = await Message.find({ chatID: chat._id })
       .sort({ createdAt: -1 })
       .limit(20)
       .select('role content')
       .lean();
 
-    // reverse the STM array to get the messages in chronological order
-    STM.reverse();
+    // reverse the stm array to get the messages in chronological order
+    stm.reverse();
 
     // create new user message in db
     await Message.create({
@@ -41,8 +50,8 @@ export const controller = {
     const res = await openai.responses.create({
       model: 'gpt-4.1-mini',
       instructions: `${SP}\n
-      STM: ${JSON.stringify(STM.map(m => ({ role: m.role, content: m.content })))}\n
-      LTM: ${JSON.stringify(chat.ltm)}`,
+      STM: ${JSON.stringify(stm.map(m => ({ role: m.role, content: m.content })))}\n
+      LTM: ${JSON.stringify(ltm)}`,
       input: request.body.query,
     });
 
@@ -56,15 +65,14 @@ export const controller = {
       role: Role.ASSISTANT,
     });
 
-    // if the LTM is provided, push it to the chat's LTM array and update the chat in db
-    if (parsedRes.ltm) chat.ltm.push(parsedRes.ltm);
-    await chat.save();
+    // update the ltm in db if the ltm is updated in the response
+    if (parsedRes.ltm) ltm.facts.push(parsedRes.ltm);
+    await ltm.save();
 
     // return the response to the client
     return response.status(200).json({
       success: true,
       message: parsedRes.answer,
-      chat: { id: chat._id, ltm: chat.ltm },
     });
   },
 };
