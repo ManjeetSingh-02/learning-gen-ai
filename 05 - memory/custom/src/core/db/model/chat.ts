fc33175721ceb1d84a9ff2c55d2a9e23a4ac6223
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-08-12 03:22:58 +0530

    chat model updated

--- 05 - memory/custom/src/core/db/model/chat.ts
+++ 05 - memory/custom/src/core/db/model/chat.ts
@@ -1,22 +1,8 @@
-// internal-imports
-import { Role } from '../../config/role.js';
-
 // external-imports
 import mongoose from 'mongoose';
 
-const messageSchema = new mongoose.Schema(
-  {
-    role: { type: String, enum: Object.values(Role), required: true, trim: true, lowercase: true },
-    content: { type: String, required: true, trim: true },
-  },
-  { _id: false, timestamps: false }
-);
-
 const chatSchema = new mongoose.Schema(
-  {
-    createdBy: { type: String, required: true, trim: true, lowercase: true },
-    messages: { type: [messageSchema], required: true },
-  },
+  { ltm: { type: [String], default: [] } },
   { timestamps: true }
 );
 
